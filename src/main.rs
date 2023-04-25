8c73f8ff0cf38fa161051e249ca34de094ec9c8e
Manal <andnothingevrything@gmail.com>
2023-04-25 14:47:57 +0200

    added simple request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+
+async fn get_request(link: &str) -> Result<(), Error> {
+    let response = reqwest::get(link).await?;
+    println!("Status: {}", response.status());
+
+    let body = response.text().await?;
+    println!("Body:\n{}", body);
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    get_request("https://www.example.com").await?;
+    Ok(())
 }
